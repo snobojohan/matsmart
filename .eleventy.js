db8ec8596a2115608b2c88718de6724a6554303f
Johan Hallberg <johan.hallberg@svt.se>
2023-12-13 01:11:14 +0100

    Responsive images

--- .eleventy.js
+++ .eleventy.js
@@ -3,11 +3,48 @@ const CleanCSS = require("clean-css");
 const UglifyJS = require("uglify-js");
 const htmlmin = require("html-minifier");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+const Image = require('@11ty/eleventy-img');
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addShortcode("image", async function (src, alt) {
+    if (alt === undefined) {
+      // You bet we throw an error on missing alt (alt="" works okay)
+      throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+    }
+
+    let metadata = await Image(src, {
+      widths: [600],
+      formats: ["jpeg"]
+    });
+
+    let data = metadata.jpeg[metadata.jpeg.length - 1];
+    return `<img src="${data.url}" width="${data.width}" height="${data.height}" alt="${alt}" loading="lazy" decoding="async">`;
+  });
 
   // Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  eleventyConfig.addNunjucksAsyncFilter("eleventyImage", async (src, options, callback) => {
+
+    // Fix error with static images
+    if (src.startsWith('/static')) {
+      src = '.' + src
+    }
+    
+    try {
+      let metadata = await Image(src, options);
+      callback(null, metadata);
+    } catch (err) {
+      callback(err);
+    }
+  });
+  
+  eleventyConfig.addNunjucksFilter("eleventyImageHtml", (metadata, options) => {
+    return Image.generateHTML(metadata, options);
+  });
 
   // Configuration API: use eleventyConfig.addLayoutAlias(from, to) to add
   // layout aliases! Say you have a bunch of existing content using
@@ -48,12 +85,12 @@ module.exports = function(eleventyConfig) {
   });
 
   // Minify CSS
-  eleventyConfig.addFilter("cssmin", function(code) {
+  eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
 
   // Minify JS
-  eleventyConfig.addFilter("jsmin", function(code) {
+  eleventyConfig.addFilter("jsmin", function (code) {
     let minified = UglifyJS.minify(code);
     if (minified.error) {
       console.log("UglifyJS error: ", minified.error);
@@ -63,7 +100,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Minify HTML output
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (outputPath.indexOf(".html") > -1) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
@@ -81,9 +118,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/css/inline.css");
 
-  /* Markdown Plugins */
-  let markdownIt = require("markdown-it");
-  let markdownItAnchor = require("markdown-it-anchor");
+  /* Markdown */
+
   let options = {
     html: true,
     breaks: true,
@@ -93,9 +129,87 @@ module.exports = function(eleventyConfig) {
     permalink: false
   };
 
-  eleventyConfig.setLibrary("md", markdownIt(options)
-    .use(markdownItAnchor, opts)
-  );
+  // Create and configure the Markdown-it instance
+  let markdownLib = markdownIt(options).use(markdownItAnchor, opts);
+
+  // TODO: Factor this out into another file
+  // Modify the renderer rules within the configured instance
+  // Thank you: https://tomichen.com/blog/posts/20220416-responsive-images-in-markdown-with-eleventy-image/
+  markdownLib.renderer.rules.image = function (tokens, idx, options, env, self) {
+    function figure(html, caption) {
+      return `<figure>${html}<figcaption>${caption}</figcaption></figure>`
+    }
+  
+    const token = tokens[idx]
+    let imgSrc = token.attrGet('src')
+    const imgAlt = token.content
+    const imgTitle = token.attrGet('title')
+  
+    const htmlOpts = { alt: imgAlt, loading: 'lazy', decoding: 'async' }
+  
+    if (imgSrc.startsWith('/assets')) {
+      imgSrc = 'src' + imgSrc
+    }
+
+    // Fix error with static images
+    if (imgSrc.startsWith('/static')) {
+      imgSrc = '.' + imgSrc
+    }
+  
+    const parsed = (imgTitle || '').match(
+      /^(?<skip>@skip(?:\[(?<width>\d+)x(?<height>\d+)\])? ?)?(?:\?\[(?<sizes>.*?)\] ?)?(?<caption>.*)/
+    ).groups
+  
+    if (parsed.skip || imgSrc.startsWith('http')) {
+      const options = { ...htmlOpts }
+      if (parsed.sizes) {
+        options.sizes = parsed.sizes
+      }
+  
+      const metadata = { jpeg: [{ url: imgSrc }] }
+      if (parsed.width && parsed.height) {
+        metadata.jpeg[0].width = parsed.width
+        metadata.jpeg[0].height = parsed.height
+      }
+  
+      const generated = Image.generateHTML(metadata, options)
+  
+      if (parsed.caption) {
+        return figure(generated, parsed.caption)
+      }
+      return generated
+    }
+
+    // TODO: add avif when support is good enough
+    // TODO: less sizes?
+  
+    const widths = [250, 316, 426, 460, 580, 768]
+    const imgOpts = {
+      widths: widths
+        .concat(widths.map((w) => w * 2)) // generate 2x sizes
+        .filter((v, i, s) => s.indexOf(v) === i), // dedupe
+      formats: ['webp', 'jpeg'], // TODO: add avif when support is good enough
+      urlPath: '/assets/img/',
+      outputDir: './_site/assets/img/'
+    }
+  
+    Image(imgSrc, imgOpts)
+  
+    const metadata = Image.statsSync(imgSrc, imgOpts)
+  
+    const generated = Image.generateHTML(metadata, {
+      sizes: parsed.sizes || '(max-width: 768px) 100vw, 768px',
+      ...htmlOpts
+    })
+  
+    if (parsed.caption) {
+      return figure(generated, parsed.caption)
+    }
+    return generated
+  }
+
+  // Set the Markdown-it instance for Eleventy
+  eleventyConfig.setLibrary("md", markdownLib);
 
   return {
     templateFormats: ["md", "njk", "html", "liquid"],
